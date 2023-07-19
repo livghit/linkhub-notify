8bf294b4db7d553fdfc4f059e7f6351914f2dc2c
livghit <liviu.g01@gmail.com>
2023-07-19 15:55:36 +0200

    Folder structures created - can change everytime :)

---
+++ api/api.go
@@ -0,0 +1,3 @@
+package api
+
+// In here we have all the stuff related to the api /api/v1/... all the routes and so on
---
+++ web/web.go
@@ -0,0 +1,3 @@
+package web
+
+// everyrthing related with views and so on routes with view in here
