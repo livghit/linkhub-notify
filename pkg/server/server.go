ba71cd292e300b483f10c626f1ec2982d4cf94e4
Liviu <108449432+livghit@users.noreply.github.com>
2023-07-20 14:56:25 +0200

    Update server.go

---
+++ pkg/server/server.go
@@ -0,0 +1,8 @@
+package server 
+
+/*
+this is the place where we define our server 
+in here we will pull the configs and render all the necesary env in order to connect to the database , check if production or not (this should go inside utils I guess but we will see )
+
+this will have a struct called Server and a method (constructor) to spin up a Server ( using the Fiber app ) 
+*/
