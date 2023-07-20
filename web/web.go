d9299e5e956e58e8fe61a4692fb181b59ace9ce8
Liviu <108449432+livghit@users.noreply.github.com>
2023-07-20 14:59:23 +0200

    Update web.go

--- web/web.go
+++ web/web.go
@@ -1,3 +1,9 @@
 package web
 
-// everyrthing related with views and so on routes with view in here
+/*
+everyrthing related with views and so on routes with view in here
+
+So I am still planing but I think here I want to put all the Routes that are related with the web what I mean by that is that all the routes that have a 'view' 
+ or simply speaking have a template to render , will have to go inside this file using the Router 
+
+*/
