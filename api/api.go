465cf6ca2b76bce6f97a79fea3bd5b5ac52c2d66
livghit <liviu.g01@gmail.com>
2023-07-22 18:44:46 +0200

    Updated api.go

--- api/api.go
+++ api/api.go
@@ -1,3 +1,18 @@
 package api
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
 // In here we have all the stuff related to the api /api/v1/... all the routes and so on
+
+var ApiRoutes map[string]func(c *fiber.Ctx);
+
+
+func LoadApiRoutes(server *fiber.App) fiber.Router{
+ // this will feed the Router with the API routes if existing 
+  api := server.Group("api/v1");
+
+  return api;
+}
+
