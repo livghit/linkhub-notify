d31084ffeeec770c313e131c6a79d2ad3cca6fe0
livghit <liviu.g01@gmail.com>
2023-07-22 20:42:40 +0200

    Some small hints for the server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,13 @@
-package server 
+package server
 
 /*
-this is the place where we define our server 
+
+this is the place where we define our server
 in here we will pull the configs and render all the necesary env in order to connect to the database , check if production or not (this should go inside utils I guess but we will see )
 
-this will have a struct called Server and a method (constructor) to spin up a Server ( using the Fiber app ) 
+this will have a struct called Server and a method (constructor) to spin up a Server ( using the Fiber app )
+
 */
+
+// the  server will have an router , server configs[link to litsen too , app name and so on]
+type Server struct{}
