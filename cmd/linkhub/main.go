9410544546460938c3403a9fc5b463089da9e20f
livghit <liviu.g01@gmail.com>
2023-07-22 20:50:41 +0200

    Future look of the app added

--- cmd/linkhub/main.go
+++ cmd/linkhub/main.go
@@ -1,14 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/livghit/linkhub/web/templates"
+)
 
 /*
 
-This will buill the app maybe I will a a bootstrap under the  utils.go Ill have to see 
+This will buill the app maybe I will a a bootstrap under the  utils.go Ill have to see
 
+This will look something like that
+
+router := router.New();
+serverConfigs := configs.LoadConfigs()
+server := server.New(serverConfigs , router)
+
+server.Run();
+
+
+QUESTIONS :
+- Where will the middleware will be placed
+- How to handle user login with azur ad
 */
 
 func main() {
-	fmt.Print("SIU")
+
+	app := fiber.New()
+
+	app.Get("/homepage", web.HomepageHandler)
+
+	log.Fatal(app.Listen(":3000"))
 }
