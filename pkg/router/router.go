1472465c8436c7408a65950d702ec7bff9252e81
livghit <liviu.g01@gmail.com>
2023-07-23 20:18:59 +0200

    Router will be instanciated inside the Server

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,11 +1,18 @@
 package router
 
+import "github.com/gofiber/fiber/v2"
 
 /*
 Kinda easy to understand im here I will create a simple router
 */
 
-type Router struct{}
+type Router struct {
+	instance *fiber.App
+}
 
+func New() *fiber.App {
+	r := Router{}
+	r.instance = fiber.New()
 
-
+	return r.instance
+}
