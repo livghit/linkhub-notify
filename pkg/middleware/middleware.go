463eb1229fab8752e53425acb3f56ac424197296
livghit <liviu.g01@gmail.com>
2023-07-27 16:25:24 +0200

    added csrf middleware

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/livghit/linkhub/pkg/config"
 )
 
@@ -10,4 +11,10 @@ func Auth() func(c *fiber.Ctx) error {
 	auth := basicauth.New(config.AuthConfig())
 
 	return auth
+}
+
+func CSRF() func(c *fiber.Ctx) error {
+	csrf := csrf.New(config.CsrfConfig())
+
+	return csrf
 }
