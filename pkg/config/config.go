a9f11fa83bffce187b4b7e75af4937edcf7a1e0b
livghit <liviu.g01@gmail.com>
2023-07-27 16:24:54 +0200

    added csrf config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/utils"
 	"github.com/livghit/linkhub/pkg/templating"
 	"github.com/livghit/linkhub/web/handlers"
 )
@@ -15,9 +19,9 @@ type TemplatingEngine struct {
 func ViewConfigs() fiber.Config {
 	// maybe do engine instead of mustache and have all the engine under engine.NewDjango NewMustache etc?
 	engine := templating.HtmlEngine()
-  engine.Reload(true)
+	engine.Reload(true)
 	configs := fiber.Config{
-		Views:       engine,
+		Views: engine,
 	}
 
 	return configs
@@ -29,8 +33,20 @@ func AuthConfig() basicauth.Config {
 			"john":  "doe",
 			"admin": "123456",
 		},
-    Unauthorized: handlers.NotAuthorized,
+		Unauthorized: handlers.NotAuthorized,
 	}
 
 	return authconfig
+}
+
+func CsrfConfig() csrf.Config {
+	csrfconfig := csrf.Config{
+		KeyLookup:      "header:X-Csrf-Token",
+		CookieName:     "csrf_",
+		CookieSameSite: "Lax",
+		Expiration:     1 * time.Hour,
+		KeyGenerator:   utils.UUID,
+	}
+
+	return csrfconfig
 }
